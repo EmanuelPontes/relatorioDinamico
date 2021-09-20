e719ee05db1718acec9a3c21fb2386c1a9f21d74
Emanuel Pontes <emanuelpp26@gmail.com>
2021-09-20 14:25:56 -0300

    inicio projeto

---
+++ src/main/java/com/empontes/relatorioDinamico/RelatorioDinamicoApplication.java
@@ -0,0 +1,13 @@
+package com.empontes.relatorioDinamico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatorioDinamicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelatorioDinamicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empontes/relatorioDinamico/RelatorioDinamicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empontes.relatorioDinamico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelatorioDinamicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
