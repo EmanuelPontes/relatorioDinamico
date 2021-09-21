3ca06c8278f82dc80cc42efd7b9b0ca67985addd
Emanuel Pontes <emanuelpp26@gmail.com>
2021-09-21 17:34:12 -0300

    Finalização das configurações iniciais e testes

--- src/main/java/com/empontes/relatorioDinamico/Model/Atributo.java
+++ src/main/java/com/empontes/relatorioDinamico/Model/Atributo.java
@@ -17,4 +17,38 @@ public class Atributo {
     private String description;
     @Column
     private Integer type;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getType() {
+        return this.type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+
 }
---
+++ src/test/java/com/empontes/relatorioDinamico/RelatorioDinamicoTests.java
@@ -0,0 +1,78 @@
+package com.empontes.relatorioDinamico;
+
+import com.empontes.relatorioDinamico.Model.Atributo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class RelatorioDinamicoTests {
+    
+
+    private static final String API_ROOT = "http://localhost:8080/rel/attrbts";
+
+    @Test
+    public void whenAll_thenOK() {
+        final Response response = RestAssured.get(API_ROOT);
+        assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+    }
+
+
+    @Test 
+    public void whenAddNew_thenAccept() {
+        final Atributo atributo = this.createRandom();
+
+        final Response response = RestAssured.given()
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .body(atributo)
+        .post(API_ROOT);
+
+        assertEquals(HttpStatus.ACCEPTED.value(), response.getStatusCode());
+
+    }
+
+    @Test 
+    public void whenDelete_thenOk() {
+        String location = this.getUriOfCreated(this.createRandom(2));
+
+        Response response = RestAssured.delete(location);
+        assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+
+        response = RestAssured.get(location);
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode());
+    }
+
+
+    private Atributo createRandom() {
+        final Atributo atributo = new Atributo();
+        atributo.setName(randomAlphabetic(10));
+        atributo.setDescription(randomAlphabetic(30));
+        atributo.setType(1);
+        return atributo;
+    }  
+    
+    private Atributo createRandom(Integer type) {
+        final Atributo atributo = new Atributo();
+        atributo.setName(randomAlphabetic(10));
+        atributo.setDescription(randomAlphabetic(30));
+        atributo.setType(type);
+        return atributo;
+    }  
+    private String getUriOfCreated(Atributo atributo) {
+        final Response response = RestAssured.given()
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .body(atributo)
+        .post(API_ROOT);
+        return API_ROOT + "/" + response.jsonPath()
+            .get("id");
+    }
+}
